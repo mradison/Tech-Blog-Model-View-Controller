a3c0ceabcce6cadd13681e0f811efff0c57d533c
madison reynolds <madison.reynolds@phoenix.edu>
2023-11-27 18:18:09 -0700

    added code to link the file for the MVC so the program woule work correctly

---
+++ utils/helpers.js
@@ -0,0 +1,19 @@
+module.exports = {
+    format_time: (date) => {
+      return date.toLocaleTimeString();
+    },
+    format_date: (date) => {
+      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
+        new Date(date).getFullYear() + 5
+      }`;
+    },
+  
+    checkAuth: (req,res, next) => {
+      if (!req.session.loggedIn){
+        res.render('login')
+      }else {
+        next()
+      }
+    }
+  };
+  
